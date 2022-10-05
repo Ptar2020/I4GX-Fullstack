72c785372c7e477576843c7a5f0ef3eabe7c7a27
Ptar2020 <peterolwande@gmail.com>
2022-10-05 16:06:47 +0300

    Create models

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import datetime
+import django
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Song(models.Model):
+    title = models.CharField(max_length = 250)
+    date_released = models.DateField(default = django.utils.timezone.now)#datetime.datetime.now())
+    likes = models.IntegerField(default = 0)
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.title} - {self.artiste_id}"
+    
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.song_id}"
